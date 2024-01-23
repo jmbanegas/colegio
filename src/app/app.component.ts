220fee5e6a0801855d4f970f999f3c094fabdb09
johanna <jmbanegas@sudamericano.edu.ec>
2024-01-23 00:46:55 -0500

    correciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,EncabezadoComponent, ContenidoComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
